3c448bf663883695cc07c9ae8718fd42439a5e43
Julián Vidal <102191030+Juli-CVidal@users.noreply.github.com>
2022-04-25 22:20:53 -0300

    Update myLinkedList.py

--- myLinkedList.py
+++ myLinkedList.py
@@ -1,146 +1,159 @@
 """nombre: Julián Vidal
 Ejercitación TAD LinkedList"""
-class LinkedList:
-	head=None
+class linkedList:
+	head = None
 
 class Node:
-	value=None
-	nextNode=None
+	value = None
+	nextNode = None
 
-def printlist(L):
-	actN=L.head
-	print("[",end="")
-	while actN!=None:
-		print (actN.value, end="")
-		if actN.nextNode!=None:
-			print("",end=", ")
-		actN=actN.nextNode
-	print ("]")
 
-def add(L,element):
-	addNode=Node()
-	addNode.value=element
-	addNode.nextNode=L.head
-	L.head=addNode
+"""Add"""
+def add(LL,element):
+	nodeToAdd = Node()
+	nodeToAdd.value = element
+	nodeToAdd.nextNode = LL.head
+	LL.head = nodeToAdd
 #Complejidad constante en lista, tad no utilizado en array
 
-def length(L):
-	leng=0
-	act=L.head
-	while act!=None:
-		act=act.nextNode
-		leng+=1
-	return leng
+"""Lenght"""
+def length(LL):
+	size = 0
+	currentNode = LL.head
+	while (currentNode != None):
+		currentNode = currentNode.nextNode
+		size += 1
+
+	return size
+
 #Complejidad lineal en lista, constante en array
 
-def search(L,element):
-	if length(L)==0:
-		return None
-	else:
-		act=L.head
-		pos=0
-		for i in range(0,length(L)):
-			if act.value==element:
-				return pos
-			else:
-				act=act.nextNode
-				pos+=1
-#Complejidad lineal, tanto en lista como en array
 
-def insert(L,element,pos):
-	act=L.head
-	actP=0 #posición actual
-	if pos==0:
-		add(L,element)
-		return pos
-	else:
-		while act!=None and actP<pos-1:
-			act=act.nextNode
-			actP+=1
-	if act==None:
-		return None
-	else:
-		insNode=Node()
-		insNode.value=element
-		insNode.nextNode=act.nextNode
-		act.nextNode=insNode
-		actP+=1
-
-		return actP
-#Complejidad lineal, tanto en lista como en array
-
-def delete(L,element):
-	pos=search(L,element)
+"""Search"""
+def search(LL,element):
+	currentNode = LL.head
 	
-	if pos==0:
-		L.head=L.head.nextNode
-	elif pos==None:
+	if (currentNode == None):
 		return None
-	else:
-		act=L.head
-		for i in range(0,pos-1):
-			act=act.nextNode
-		act.nextNode=act.nextNode.nextNode
-		
-		return pos
+
+	index = 0
+	size = length(LL)
+	for i in range(size):
+		if (currentNode.value == element):
+			return index
+
+		currentNode = currentNode.nextNode
+		index += 1
+	return None
+
 #Complejidad lineal, tanto en lista como en array
 
-def access(L,pos):
-	act=L.head
-	for i in range(0,pos):
-		if act==None: break
-		else:
-			act=act.nextNode
-	if act==None:
+
+"""Insert"""
+def insert(LL,element,index):
+	if (index == 0):
+		add(LL,element)
+		return 0
+
+	prevNode = getNode(LL,index - 1)
+	if (prevNode == None):
 		return None
-	else:
-		return act.value
+
+	nodeToInsert = Node()
+	nodeToInsert.value = element
+	nodeToInsert.nextNode = prevNode.nextNode
+	prevNode.nextNode = nodeToInsert
+	
+	return index
+#Complejidad lineal, tanto en lista como en array
+
+
+"""Delete"""
+def delete(LL,element):
+	index = search(LL,element)
+	return delete_by_pos(LL,index)
+
+
+def delete_by_pos(LL,index):
+	if (index == None):
+		return None
+	if (index == 0):
+		LL.head = LL.head.nextNode
+		return 0
+
+	prevNode = getNode(LL,index - 1)
+	prevNode.nextNode = prevNode.nextNode.nextNode
+	return index
+#Complejidad lineal, tanto en lista como en array
+
+"""Access"""
+def access(LL,index):
+	currentNode = getNode(LL,index - 1)
+	if (currentNode == None or currentNode.nextNode == None):
+		return None
+
+	return currentNode.nextNode.value
+	
 #Complejidad lineal en lista, constante en array
 
-def update(L,element,pos):
-	act=L.head 	#nodo actual
-	actP=0			#posición actual
-	while actP<pos and act!=None:
-		act=act.nextNode
-		actP+=1
-	
-	if act==None:
+
+"""Update"""
+def update(LL,element,index):
+	currentNode = getNode(LL,index)
+	if (currentNode == None):
 		return None
-	else:
-		act.value=element
-		return actP
+
+	currentNode.value = element
+	return index
 #Complejidad lineal en lista, tad no implementado en array
 
-def reverse(L):
-	prev=None #nodo anterior
-	act=L.head #nodo actual
-	nextt=act.nextNode #nodo siguiente
-	while act!=None:
-		act.nextNode=prev
-		prev=act
-		act=nextt
-		if nextt!=None:
-			nextt=nextt.nextNode
-	L.head=prev
+
+"""Auxiliares"""
+def getNode(LL,index):
+	currentNode = LL.head
+	currentIndex = 0
+
+	while (currentNode != None):
+		if (currentIndex == index):
+			return currentNode
+		currentNode = currentNode.nextNode
+		currentIndex += 1
+
+	return None
+
+def getIndex(LL,node):
+	index = 0
+	currentNode = LL.head
+	
+	while (currentNode != None):
+		if (currentNode == node):
+			return index
+		index += 1
+		currentNode = currentNode.nextNode
+
+	return 0
 
 
-def enqueue(Q,element):
-	add(Q,element)
+def reverse(LL):
+	prevNode = None 
+	currentNode = LL.head 
+	nextNode = currentNode.nextNode 
+	while (currentNode != None):
+		currentNode.nextNode = prevNode
+		prevNode = currentNode
+		currentNode = nextNode
+		if (nextNode != None):
+			nextNode = nextNode.nextNode
+
+	LL.head = prevNode
 
 
-def dequeue(Q):
-	curr=Q.head
-	if curr==None:
-		return None
-	elif curr.nextNode==None:
-		Q.head=None
-		return curr.value
-	else:
-		prev=Q.head
-		curr=Q.head.nextNode
-		while curr.nextNode!=None:
-			prev=curr
-			curr=curr.nextNode
-		prev.nextNode=None
-
-		return curr.value
+def printList(LL):
+	currentNode = LL.head
+	print("[", end="")
+	while (currentNode != None):
+		print(currentNode.value, end="")
+		if (currentNode.nextNode != None):
+			print("",end=", ")
+		currentNode = currentNode.nextNode
+	print("]")
